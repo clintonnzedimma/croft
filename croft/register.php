c7f0fbd7c698463dffc711cef6b4a4a92d77b201
Clinton Nzedimma <34009341+clintonnzedimma@users.noreply.github.com>
2018-08-16 18:15:28 +0100

    Add files via upload

---
+++ croft/register.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Register Car</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">	
+</head>
+<body>
+
+<div class="register-box">
+	<h2 align="center">Register a car</h2>
+	<div class="form-holder" align="center">	
+		<form>
+			Plate Number: <br>
+			<input type="text" name="plate-number">
+			<br>
+			Color: <br>
+			<input type="text" name="color">
+			<br>
+			Model: <br>
+			<input type="text" name="model">
+			<br>
+			Chasis Number: <br>
+			<input type="text" name="chasis-number">
+			<br>
+			Issue Date: <br>
+			<input type="text" name="issue-date">
+			<br>
+			<br>
+			<input type="submit" name="ok" value="Register Car">
+		</form>
+	</div>
+	
+</div>
+
+
+</body>
+</html>
---
+++ croft/search.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Register Car</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">	
+</head>
+<body>
+
+<div class="search-result">
+	<h2 align="center">Results</h2>
+	<div class="form-holder" align="center">	
+		<form>
+			<p>
+				<b>Plate Number</b>: {fuck}
+			</p>
+
+			<p>
+				<b>Color</b>: {fuck}
+			</p>
+
+			<p>
+				<b>Model</b>:{fuck}
+			
+			</p>
+
+			<p>
+				<b>Chasis Number </b>:{fuck}
+			</p>
+
+			<p>
+				<b>Issue Date</b>: {fuck}		
+			</p>	
+
+		</form>
+	</div>
+	
+</div>
+
+
+</body>
+</html>
